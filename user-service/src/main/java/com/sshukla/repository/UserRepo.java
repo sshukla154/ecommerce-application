dd86c2254bed237dad9f971749f096bf97b1c68e
Seemant Shukla <13106945+sshukla154@users.noreply.github.com>
2023-05-08 19:42:41 +0200

    Working - Refactoring

--- user-service/src/main/java/com/sshukla/repository/UserRepo.java
+++ user-service/src/main/java/com/sshukla/repository/UserRepo.java
@@ -2,8 +2,7 @@ package com.sshukla.repository;
 
 import com.sshukla.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
@@ -11,8 +10,7 @@ import java.util.Optional;
  * Created by `Seemant Shukla` on 07-05-2023
  */
 
-//@Repository
+@Repository
 public interface UserRepo extends JpaRepository<User, String> {
-	@Query("SELECT u FROM User u WHERE u.username = :username")
-	Optional<User> findByUsername(@Param("username") String username);
+	Optional<User> findByUsername(String username);
 }
